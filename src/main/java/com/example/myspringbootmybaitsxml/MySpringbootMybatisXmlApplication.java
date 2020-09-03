3a26197c2ade8e02f13b1aa7e359349b9d70f479
zhoupeng <287214764@qq.com>
2020-09-03 09:39:22 +0800

    配置druid数据源及监控

--- src/main/java/com/example/myspringbootmybaitsxml/MySpringbootMybatisXmlApplication.java
+++ src/main/java/com/example/myspringbootmybaitsxml/MySpringbootMybatisXmlApplication.java
@@ -2,12 +2,23 @@ package com.example.myspringbootmybaitsxml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class MySpringbootMybatisXmlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySpringbootMybatisXmlApplication.class, args);
+        ConfigurableApplicationContext ca = SpringApplication.run(MySpringbootMybatisXmlApplication.class, args);
+        DataSource dataSource = (DataSource) ca.getBean("dataSource");
+        try {
+            System.out.println(dataSource);
+            System.out.println(dataSource.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
